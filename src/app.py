bcfd5e2409dccc03d6861b44ba4b26d297947e98
LuLu-89 <lucy.ly89@gmail.com>
2019-08-06 13:23:14 -0600

    update routes

--- src/app.py
+++ src/app.py
@@ -18,10 +18,20 @@ session = Session(engine)
 # Flask setup
 app = Flask(__name__)
 
-# Route to Data page only
+# Routes:
+@app.route('/')
+def index():
+    return 'Hello welcome to Lucys app'
+
 @app.route('/data')
 def data():
-    return jsonify(data)
+    return 'Select a date'
+
+@app.route('/data/<date_iso_format>')
+def get_data(date_iso_format):
+    data_for_that_date = session.query(date_iso_format)
+    # data_for_that_date = temporary_date_data[date_iso_format]
+    return jsonify(list(data_for_that_date))
 
 
 if __name__ == '__main__':
