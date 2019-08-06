5ab740e25fc3b0abd0e5dc50a44f44fe0dd98fcc
LuLu-89 <lucy.ly89@gmail.com>
2019-08-06 00:47:28 -0600

    adding buttons and checkboxes... not working

--- src/components/DayDataForm/DayDataForm.js
+++ src/components/DayDataForm/DayDataForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import moment from 'moment';
-
 import {
     div,
     Route,
     Link,
 } from 'react-router-dom';
+import '../style.css'
 
 import DataAPI from '../../api';
 
@@ -16,11 +16,11 @@ class DayDataFormComponent extends React.Component {
         //--
         super(props);
 
-        // we will populate the `state`, and *change* the `state` depending on which date the URL is currently pointing at
+        // we will populate the `state`, and *change* the `state` 
+        // depending on which date the URL is currently pointing at
         this.state = {
             date_iso_format: undefined,
             data: {}
-
         };
 
     }
@@ -29,6 +29,44 @@ class DayDataFormComponent extends React.Component {
     // > Called immediately after a component is mounted. 
     // > Setting state here will trigger re-rendering.
     // we will set state to update and show new data
+
+    selectAllCheckboxes = isSelected => {
+        Object.keys(this.state.checkboxes).forEach(checkbox => {
+            // BONUS: Can you explain why we pass updater function to setState instead of an object?
+            this.setState(prevState => ({
+                checkboxes: {
+                    ...prevState.checkboxes,
+                    [checkbox]: isSelected
+                }
+            }));
+        });
+    };
+
+    selectAll = () => this.selectAllCheckboxes(true);
+
+    deselectAll = () => this.selectAllCheckboxes(false);
+
+    handleCheckboxChange = changeEvent => {
+        const { name } = changeEvent.target;
+
+        this.setState(prevState => ({
+            checkboxes: {
+                ...prevState.checkboxes,
+                [name]: !prevState.checkboxes[name]
+            }
+        }));
+    };
+
+    handleFormSubmit = formSubmitEvent => {
+        formSubmitEvent.preventDefault();
+
+        Object.keys(this.state.checkboxes)
+            .filter(checkbox => this.state.checkboxes[checkbox])
+            .forEach(checkbox => {
+                console.log(checkbox, "is selected.");
+            });
+    };
+
 
     //https://egghead.io/lessons/javascript-redux-fetching-data-on-route-change
     // componentDidMount() {
@@ -62,30 +100,87 @@ class DayDataFormComponent extends React.Component {
     }
 
     render() {
-        const feature_1__val = this.state.data.feature_1 === undefined ? 'N/A' : this.state.data.feature_1.toString();
-        const feature_2__val = this.state.data.feature_2 === undefined ? 'N/A' : this.state.data.feature_2.toString();
-        const feature_3__val = this.state.data.feature_3 === undefined ? 'N/A' : this.state.data.feature_3.toString();
-        const target__val = this.state.data.target === undefined ? 'N/A' : this.state.data.target.toString();
+        const stress__val = this.state.data.stress === undefined ? 'N/A' : this.state.data.stress.toString();
+        const lackSleep__val = this.state.data.lackSleep === undefined ? 'N/A' : this.state.data.lackSleep.toString();
+        const workout__val = this.state.data.workout === undefined ? 'N/A' : this.state.data.workout.toString();
+        const event__val = this.state.data.event === undefined ? 'N/A' : this.state.data.event.toString();
 
         return (<div className="day-data-container">
             <h1>
                 {this.getDateIsoFormatFromRoute()}
             </h1>
-            <div>
-                <h3><label> Feature 1: </label> {feature_1__val}</h3>
-                <h3> <label> Feature 2: </label> {feature_2__val}</h3>
-                <h3><label> Feature 3: </label> {feature_3__val}</h3>
-                <hr />
-                <h3><label> Target: </label> {target__val}</h3>
+            <div className="row mt-5">
+                <div className="col-sm-12">
+                    <form onSubmit={this.handleFormSubmit}>
+                        <div>
+                            <h3>
+                                <label className='checkbox'> stress:
+                    {/* onChange={onCheckboxChange} */}
+                                    <input
+                                        type="checkbox"
+                                        name="stress"
+                                        checked={this.state.data.stress}
+                                        className="form-check-input"
+                                    />
+                                </label>
+                            </h3>
+                            <h3>
+                                <label> lack sleep:
+                    <input
+                                        type="checkbox"
+                                        name="lack sleep"
+                                        checked={this.state.data.lackSleep}
+                                        className="form-check-input" />
+                                </label>
+                            </h3>
+                            <h3>
+                                <label> workout:
+                    <input
+                                        type="checkbox"
+                                        name="workout"
+                                        checked={this.state.data.workout}
+                                        className="form-check-input" />
+                                </label>
+                            </h3>
+                            <hr />
+                            <h3>
+                                <label> event:
+                    <input
+                                        type="checkbox"
+                                        name="event"
+                                        checked={this.state.data.event}
+                                        className="form-check-input" />
+                                </label>
+                            </h3>
+                            <div className="form-group mt-2">
+                                <button
+                                    type="button"
+                                    className="btn btn-outline-primary mr-2"
+                                    onClick={this.selectAll}
+                                >
+                                    Select All
+                </button>
+                                <button
+                                    type="button"
+                                    className="btn btn-outline-primary mr-2"
+                                    onClick={this.deselectAll}
+                                >
+                                    Deselect All
+                </button>
+                                <button type="submit" className="btn btn-primary">
+                                    Save
+                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>);
-
     }
-
 }
 
 export default (props) => {
     // https://flaviocopes.com/react-pass-props-to-children/
     // 
-    return <DayDataFormComponent {...props}></DayDataFormComponent>
+    return <CalendarComponent {...props}></CalendarComponent>
 }
