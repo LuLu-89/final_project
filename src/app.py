ee7492e4aba447c1052c755da117b232d0adc87f
LuLu-89 <lucy.ly89@gmail.com>
2019-08-06 23:01:18 -0600

    trying to add ML concept

--- src/app.py
+++ src/app.py
@@ -4,8 +4,10 @@ import pandas as pd
 import sqlalchemy
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from sklearn.linear_model import LogisticRegression
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, redirect
+import event_predictor
 
 
 # Setup
@@ -18,20 +20,34 @@ session = Session(engine)
 # Flask setup
 app = Flask(__name__)
 
+# temporary_date_data = {
+#         '2019-08-02': {
+#             'stress': False,
+#             'lackSleep': False,
+#             'workout': False
+#         }
+# }
+
 # Routes:
 @app.route('/')
 def index():
-    return 'Hello welcome to Lucys app'
+    event = engine.find_one()
+    return render_template('', event=event_predictor)
+
 
 @app.route('/data')
 def data():
+    _df = _df.append(new_observation, ignore_index=True)
+    # len(_df)
+    event_data = event_predictor.data()
+    event.update({_df}, event_data, upsert=True)
     return 'Select a date'
 
 @app.route('/data/<date_iso_format>')
 def get_data(date_iso_format):
     data_for_that_date = session.query(date_iso_format)
     # data_for_that_date = temporary_date_data[date_iso_format]
-    return jsonify(list(data_for_that_date))
+    return jsonify(data_for_that_date)
 
 
 if __name__ == '__main__':
