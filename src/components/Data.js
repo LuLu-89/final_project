3d37b0f0cdc5d9934eb112c477f8b0478d05ee01
LuLu-89 <lucy.ly89@gmail.com>
2019-08-11 00:29:21 -0600

    try to plug in seed data but i did something wrong

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,11 +1,76 @@
 import React from 'react';
-import MyDropdown from './MyDropdown';
 
-const test = () => {
-  return (
-    <div >Data component</div>
-  )
-  
+
+// https://stackoverflow.com/questions/41374572/how-to-render-an-array-of-objects-in-react?rq=1
+class DataComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    const DataAPI = [
+      ('date_data': {
+        '2019-08-02': {
+          // your `X`
+          stress: false,
+          lackSleep: false,
+          workout: false,
+
+          // your `y`
+          event: true
+        },
+
+        '2019-08-03': {
+          // your `X`
+          stress: false,
+          lackSleep: true,
+          workout: false,
+
+          // your `y`
+          event: true
+        },
+
+        '2019-08-04': {
+          // your `X`
+          stress: false,
+          lackSleep: false,
+          workout: false,
+
+          // your `y`
+          event: true
+        },
+
+        '2019-08-05': {
+          // your `X`
+          stress: true,
+          lackSleep: true,
+          workout: false,
+
+          // your `y`
+          event: false
+        },
+
+        '2019-08-06': {
+          // your `X`
+          stress: false,
+          lackSleep: true,
+          workout: true,
+
+          // your `y`
+          event: true
+        }
+        
+      })];
+
+
+    return (
+      <div>
+        {DataAPI.map(function(d, idx) {
+          return (<li key = {idx}>{d.data_date}</li>)
+        })}
+      </div>
+    );
+  }
 }
-  
-export default test
+
+export default DataComponent
